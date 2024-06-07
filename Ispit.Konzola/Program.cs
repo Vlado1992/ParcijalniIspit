7035b60a712f6f253b03713842da79fd51d3cbd1
Vlado1992 <vladimir.veljkovic.os@gmail.com>
2024-06-07 18:56:41 +0200

    Add project files.

---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ispit.Model;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ucenik> ucenici = new List<Ucenik>();
+                       
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Unesite podatke za učenika {i + 1}:");
+
+                Console.WriteLine("Unesite ime učenika:");
+                string ime = Console.ReadLine();
+
+                Console.WriteLine("Unesite prezime učenika:");
+                string prezime = Console.ReadLine();
+
+                Console.WriteLine("Unesite datum rođenja učenika (u formatu dd-mm-yyyy):");
+                DateTime datumRodjenja;
+                while (!DateTime.TryParse(Console.ReadLine(), out datumRodjenja))
+                {
+                    Console.WriteLine("Unesite valjani datum rođenja (u formatu dd-mm-yyyy):");
+                }
+
+                Console.WriteLine("Unesite prosjek učenika:");
+                double prosjek;
+                while (!double.TryParse(Console.ReadLine(), out prosjek) || prosjek < 1 || prosjek > 5)
+                {
+                    Console.WriteLine("Unesite valjani prosjek (od 1 do 5):");
+                }
+
+                Ucenik ucenik = new Ucenik(ime, prezime, datumRodjenja, prosjek);
+                ucenici.Add(ucenik);
+            }
+
+            Console.WriteLine("\nPodaci o učeniku:\n");
+            foreach (Ucenik ucenik in ucenici)
+            {
+                ucenik.IspisiPodatke();
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ispit.Model/Ucenik.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Ucenik
+    {
+        public string Ime { get; set; }
+
+        public string Prezime { get; set; }
+
+        public DateTime DatumRodjenja { get; set; }
+
+        public double Prosjek { get; set; }
+
+        public Ucenik(string ime, string prezime, DateTime datumRodjenja, double prosjek)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodjenja = datumRodjenja;
+            Prosjek = prosjek;
+        }
+
+        public int Starost()
+        {
+            DateTime danasnjiDatum = DateTime.Now;
+            int godineUcenika = danasnjiDatum.Year - DatumRodjenja.Year;
+            if (danasnjiDatum < DatumRodjenja.AddYears(godineUcenika))
+            {
+                godineUcenika--;
+            }
+            return godineUcenika;
+        }
+
+        public string UkupanProsjek()
+        {
+            if (Prosjek >= 1 && Prosjek <= 1.49)
+            {
+                return "Nedovoljan!";
+            }
+            else if (Prosjek >= 1.50 && Prosjek <= 2.49)
+            {
+                return "Dovoljan!";
+            }
+            else if (Prosjek >= 2.50 && Prosjek <= 3.49)
+            {
+                return "Dobar!";
+            }
+            else if (Prosjek >= 3.50 && Prosjek <= 4.49)
+            {
+                return "Vrlo dobar!";
+            }
+            else if (Prosjek >= 4.50 && Prosjek <= 5.00)
+            {
+                return "Odličan!";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void IspisiPodatke()
+        {           
+            Console.WriteLine($"Ime: {Ime}");
+            Console.WriteLine($"Prezime: {Prezime}");
+            Console.WriteLine($"Datum Rođenja: {DatumRodjenja.ToString("dd-mm-yyyy")}");
+            Console.WriteLine($"Starost: {Starost()} godina");
+            Console.WriteLine($"Prosjek: {Prosjek} - {UkupanProsjek()}");
+            Console.WriteLine("--------------------------------------");
+        }
+    }
+}
